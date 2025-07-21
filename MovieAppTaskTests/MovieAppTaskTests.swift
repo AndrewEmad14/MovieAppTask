4037a1dd9085ae60d41ff1167be0db64cdc2e450
Andrew <andrew14emad@gmail.com>
2025-07-21 09:07:05 -0700

    Initial Commit

---
+++ MovieAppTaskTests/MovieAppTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTaskTests.swift
+//  MovieAppTaskTests
+//
+//  Created by Andrew Emad Morris on 21/07/2025.
+//
+
+import Testing
+@testable import MovieAppTask
+
+struct MovieAppTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
